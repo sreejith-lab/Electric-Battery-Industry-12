447d806c2971a096bd2c5d4182ea42c63a13dc3f
Sreejith <sreejith@thestackly.com>
2025-10-15 12:21:30 +0530

    18

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+const menu = document.getElementById('menu');
+const aside = document.querySelector('aside');
+menu.addEventListener('click',()=>{
+    aside.classList.toggle('aside1');
+if  (aside.classList.contains('aside1')){
+    menu.classList.remove('fa-bars');
+    menu.classList.add('fa-x');
+}else{
+    menu.classList.remove('fa-x');
+    menu.classList.add('fa-bars')
+}
+})
+
+///////////////////////////////
 function openid(id) {
     // Map li IDs to corresponding card IDs
     const map = {
